de9d9bc8627b573eff6739ccc90ef004d98cf55f
Kleshnev Anton <antongloom89@gmail.ru>
2019-05-12 21:55:28 +0300

    #2 commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  	<div class="wrapper">
+  		<header>
+			<div class="navbar">
+			  <div class="container">
+			    <div class="navbar-content"><a class="header-logo" href="index.html">UImini</a>
+			      <div class="button-burger"><span class="line line-1"></span><span class="line line-2"></span><span class="line line-3"></span></div>
+			      <div class="navbar-list__wrapper">
+			        <ul class="navbar-list">
+			          <li class="navbar-item"><a class="navbar-link" href="get-started.html">Documetation</a></li>
+			          <li class="navbar-item"><a class="navbar-link" href="changelog.html">Changelog</a></li>
+			          <li class="navbar-item"><a class="navbar-link" href="get-started.html">Download</a></li>
+			        </ul>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+  		</header>	
+  		<div class="content-wrapper">
+  			<h1>vsdv</h1>
+  		</div>
+  	</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data(){
+  	return {
+  		
+  	}
+  }
 }
 </script>
 
-<style>
+<style lang="stylus">
+@import './assets/stylus/main.styl'
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
