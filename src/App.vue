b5d2b31ab8a7ecae988e9dc1e0525d0bfa958b52
Kleshnev Anton <antongloom89@gmail.ru>
2019-05-19 20:59:07 +0300

    #2commit

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,19 @@
   <div id="app">
   	<div class="wrapper">
   		<header>
-			<div class="navbar">
-			  <div class="container">
-			    <div class="navbar-content"><a class="header-logo" href="index.html">UImini</a>
-			      <div class="button-burger"><span class="line line-1"></span><span class="line line-2"></span><span class="line line-3"></span></div>
-			      <div class="navbar-list__wrapper">
-			        <ul class="navbar-list">
-			          <li class="navbar-item"><a class="navbar-link" href="get-started.html">Documetation</a></li>
-			          <li class="navbar-item"><a class="navbar-link" href="changelog.html">Changelog</a></li>
-			          <li class="navbar-item"><a class="navbar-link" href="get-started.html">Download</a></li>
-			        </ul>
-			      </div>
-			    </div>
-			  </div>
-			</div>
+				<div class="navbar">
+				  <div class="container">
+				    <div class="navbar-content">
+				    	<router-link class="header-logo" to="/">UImini</router-link>
+				      <div class="button-burger"><span class="line line-1"></span><span class="line line-2"></span><span class="line line-3"></span></div>
+				      <div class="navbar-list__wrapper">
+				        <ul class="navbar-list">
+				          <li class="navbar-item"><a class="navbar-link" href="get-started.html">Documetation</a></li>
+				        </ul>
+				      </div>
+				    </div>
+				  </div>
+				</div>
   		</header>	
   		<div class="content-wrapper">
   			<h1>vsdv</h1>
